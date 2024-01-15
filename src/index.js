6b97db5609588a3e0887661c0de70e8881d4b0b3
JonatanDrysen <jojodrysen@gmail.com>
2024-01-15 13:56:15 +0100

    add task 3

--- src/index.js
+++ src/index.js
@@ -8,7 +8,28 @@ function greetingWithName(userName) {
 }
 
 // takes in the arguments 2 & 6 from index.html and passes it to the fn
-function addTwoNumbers(x, y) { 
+function addTwoNumbers(x, y) {
     const sum = x + y
     console.log("The sum of", x, "and", y, "is: ", sum)
+}
+
+
+// checks if the arguments are valid, if so it passes them to divideTwoNumbers()
+function divideTwoNumbersPrompt() {
+    const x = prompt("Enter the first number", "0")
+    const y = prompt("Enter the second number", "0")
+    if(isNaN(x) || isNaN(y)) {
+        console.log("The input must be numbers!")
+        return;
+    }
+    divideTwoNumbers(x, y)
+}
+
+// takes in the arguments as parameters, checks for zeroes, if not, logs quotient.
+function divideTwoNumbers(x, y) {
+    if (x == 0 || y == 0) {
+        console.log("You cannot divide by 0!")
+        return;
+    }
+    console.log(`${x} divided by ${y} is ${x / y}`)
 }
